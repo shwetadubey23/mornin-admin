9beba19e126a40f17f8e0867e999d2fbbf423dee
akanksha089 <achaturvdi089@gmail.com>
2023-11-18 18:16:31 +0530

    save

--- src/pages/MessageCategory/components/UpdateCategoryModal/UpdateCategoryModal.js
+++ src/pages/MessageCategory/components/UpdateCategoryModal/UpdateCategoryModal.js
@@ -25,7 +25,7 @@ export default function UpdateCategoryModal(props) {
 
         <div className="p-4">
           <button className="text-base text-gray-600 p-4 py-2 border border-gray-300 border-b-transparent bg-white -mb-0.5 font-bold">
-            Update Message
+          Update Message Category
           </button>
           <form autoComplete="off" className="p-4 space-y-4 capitalize border border-gray-300 ">
 
